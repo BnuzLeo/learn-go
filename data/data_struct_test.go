dbb3846b3ae498abc60169634ae067b740a6dd5d
bnuzleon <bnuzleon@163.com>
2021-03-11 00:10:08 +0800

    第四章： 字符串

--- data/data_struct_test.go
+++ data/data_struct_test.go
@@ -1,1 +1,17 @@
 package data
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+	字符串：不可变的字节序列
+	默认UTF-8编码，存储Unicode字符，字面量允许使用十六进制，八进制和UTF编码格式
+*/
+func TestString(t *testing.T) {
+	s := "震宇\x61\142\u0041"
+
+	fmt.Printf("%s\n", s)
+	fmt.Printf("% x, len: %d \n", s, len(s))
+}
